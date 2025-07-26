78d2bec19dc15983be12a71dad46eed298899967
Zach Manson <zachpmanson@gmail.com>
2025-07-26 18:49:24 +1000

    fix: make debug absolute

--- components/cards/DebugModal.tsx
+++ components/cards/DebugModal.tsx
@@ -6,7 +6,11 @@ export default function DebugModal({ children }: { children: React.ReactNode }) 
   const [open, setOpen] = useState(false);
   return (
     <>
-      <Button color="white" onPress={() => setOpen(true)} style={{ backgroundColor: "red", width: 100 }}>
+      <Button
+        color="white"
+        onPress={() => setOpen(true)}
+        style={{ backgroundColor: "red", width: 100, position: "absolute", top: 0, right: 0 }}
+      >
         Debug
       </Button>
       <Modal visible={open} animationType="slide" style={{ backgroundColor: "white", alignItems: "stretch" }}>
